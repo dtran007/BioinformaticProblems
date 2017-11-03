01ac70cad3ed12da6964736eefdbb497c5a02137
Danny <dtl2an@gmail.com>
2017-11-02 22:13:11 -0700

    Counting DNA Nucleotides Problem

---
+++ DNA/DNA.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+
+#Counting DNA Nucleotides
+
+import sys
+
+fname = sys.argv[-1]
+
+f = open(fname, "r")
+
+instring = f.readline()
+
+A=0
+C=0
+G=0
+T=0
+
+for i in range(0, len(instring)):
+    if  instring[i] == 'A':
+        A += 1
+    elif instring[i] == 'C':
+        C += 1
+    elif instring[i] == 'G':
+        G += 1
+    elif instring[i] == 'T':
+        T += 1
+
+print("{0} {1} {2} {3}").format(A, C, G, T)
+
+f.close()
