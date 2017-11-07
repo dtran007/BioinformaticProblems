cec2ac952043d7856e3d99299fc30ccde0520eb6
Danny <dtl2an@gmail.com>
2017-11-06 17:11:44 -0800

    Reverse Complement of DNA

---
+++ REVC/Complement.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+
+#Reverse complement of DNA string
+#Note the directionality of the strands
+#   5' --- 3'
+#   3' --- 5'
+
+import sys
+
+fname = sys.argv[-1]
+
+f = open(fname, "r")
+
+instring = f.readline()
+
+Comp = ""
+
+for i in range(0, len(instring)):
+    if instring[i] == 'A':
+        Comp += 'T'
+    elif instring[i] == 'T':
+        Comp += 'A'
+    elif instring[i] == 'C':
+        Comp += 'G'
+    elif instring[i] == 'G':
+        Comp += 'C'
+
+revComp = Comp[::-1]
+
+print(revComp)
+
+
